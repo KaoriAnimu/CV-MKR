a0d7e0ff05e81e2112a7c0c6204cb7be011b1e6f
Koi <62371137+KaoriAnimu@users.noreply.github.com>
2025-05-16 16:09:12 +0700

    Styling fade in, fade out, also Background image good

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ const fadeSections = document.querySelectorAll(
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    }
-  });
+  entry.target.classList.add('visible');
+    } else {
+  entry.target.classList.remove('visible');
+} });
 }, {
   threshold: 0.1
 });
